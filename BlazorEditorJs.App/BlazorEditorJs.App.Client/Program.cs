fc7a9e5589050f5a9099d475e9c413832ae0876d
Kibble White <kibblewhite@live.com>
2024-02-11 21:52:58 +0000

    Reshuffling of files, added new Blazor WASM client application for showcasing

---
+++ BlazorEditorJs.App/BlazorEditorJs.App.Client/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorEditorJs.App.Client;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        await builder.Build().RunAsync();
+    }
+}
